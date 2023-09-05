216b645c0ac90834cd9b5bd75d3cb1e5bc13f6e7
Adrian Barnett <a.barnett@qut.edu.au>
2023-09-05 21:12:42 +1000

    poche 2

--- Poche/to_pdf.R
+++
@@ -1,7 +0,0 @@
-# make PDF version
-library(renderthis)
-chrome_on_my_pc = "C:/Program Files (x86)/Google/Chrome/Application/chrome.exe"
-Sys.setenv(PAGEDOWN_CHROME = chrome_on_my_pc)
-Sys.setenv(CHROMOTE_CHROME = chrome_on_my_pc)
-to_pdf(from = "slides.Rmd")
-
